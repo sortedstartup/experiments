8063307d65436e36d7ceb81acc7ccd2a1758edac
Swapnil Srivastava <142904704+Swapnilden@users.noreply.github.com>
2025-06-15 15:28:12 +0000

    added code to create system to check expiry of domain and send email

---
+++ send-email-poc/notifier.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
+
+func sendWithSendGrid(to, subject, body string) error {
+	apiKey := os.Getenv("SENDGRID_API_KEY")
+	sender := os.Getenv("EMAIL_SENDER")
+
+	if apiKey == "" || sender == "" {
+		return fmt.Errorf("SendGrid API key or sender email not set")
+	}
+
+	from := mail.NewEmail("Domain Monitor", sender)
+	toEmail := mail.NewEmail("", to)
+	message := mail.NewSingleEmail(from, subject, toEmail, body, body)
+
+	client := sendgrid.NewSendClient(apiKey)
+	response, err := client.Send(message)
+
+	if err != nil {
+		return fmt.Errorf("SendGrid send error: %v", err)
+	}
+
+	if response.StatusCode >= 400 {
+		return fmt.Errorf("SendGrid error: %s", response.Body)
+	}
+
+	return nil
+}
