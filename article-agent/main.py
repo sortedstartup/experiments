ae9e3462bf66a2bd28398421278707e8df6a33a4
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2025-12-09 19:33:29 +0530

    added agent instruction

--- article-agent/main.py
+++ article-agent/main.py
@@ -25,7 +25,21 @@ async def main():
         ) as brave_server:
             agent = Agent(
                 name="Web Agent",
-                instructions="You provide assistance with web scraping and search queries. Use Playwright for navigating websites and extracting data. Use Brave Search for searching the web.",
+                instructions="""You are an **Article Creator Agent**. 
+                Your main job is to take a topic and create an article about it.
+
+                You can use the following tools:
+                - Brave Search: To search for information on the web.
+                - Playwright: To scrape the web.
+
+                Steps to follow for creating an article from the users requirements:
+                1. **Search:**
+                    - Use Brave Search to search for information on the web use TOP 2 results.
+                2. **Scrape:**
+                    - Use Playwright to scrape the web.
+                3. **Write:**
+                    - Use the information from the web to write an article.
+                """,
                 mcp_servers=[playwright_server, brave_server],
                 model="gpt-5-mini-2025-08-07",
             )
