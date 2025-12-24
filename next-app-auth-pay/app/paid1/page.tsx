8b30676cb4856275abb96734de6196f7edf29377
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2025-12-24 03:02:53 +0000

    paid content pages

---
+++ next-app-auth-pay/app/paid1/page.tsx
@@ -0,0 +1,7 @@
+export default function Paid1() {
+  return (
+    <div>
+      <h1>Paid 1</h1>
+    </div>
+  );
+}
---
+++ next-app-auth-pay/app/paid2/page.tsx
@@ -0,0 +1,7 @@
+export default function Paid2() {
+  return (
+    <div>
+      <h1>Paid 2</h1>
+    </div>
+  );
+}
